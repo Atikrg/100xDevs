fe1cd886c5face2e3545c4a3672798f97d5e4ce5
atikrangnekar <atikrangnekar28@gmail.com>
2026-01-24 09:26:15 +0530

    course-selling api prisma setup

---
+++ web_dev/course_selling_api/index.ts
@@ -0,0 +1,9 @@
+import {app} from "./app";
+import { prisma } from "./db";
+
+const PORT = process.env.SERVER_PORT;
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}`)
+})
